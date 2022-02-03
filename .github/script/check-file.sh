a8756e50bcd76f2670a2b014795a08f5c5c1ec43
Kevin Heis <heiskr@users.noreply.github.com>
2022-02-03 11:50:51 -0800

    Create check-file.sh

---
+++ .github/script/check-file.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Make sure this file is executable
+# chmod a+x .github/script/check-file.sh
+
+echo "Check that $FILE includes $SEARCH"
+if grep --extended-regexp $SEARCH $FILE
+then
+  echo "Found $SEARCH in $FILE"
+else
+  echo "Missing $SEARCH in $FILE"
+  echo "----------------"
+  echo "$(cat $FILE)"
+  exit 204  # We're sending a weird code so it looks different from other "failures"
+fi
